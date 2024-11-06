e38c6d50ae44ba7d0ab449f4ca38a365f1849cd7
Jarrod <jarrod@span-eng.com>
2024-11-07 09:10:21 +1000

    remote tile update

--- remote-component/src/DashboardGrid.js
+++ remote-component/src/DashboardGrid.js
@@ -5,12 +5,12 @@ import TitleBar from './WellTile';
 const DashboardGrid = () => {
   // Sample data for the six tiles
   const tileData = [
-    { title: 'Well 1', tankLevel: 50, actualFlowRate: 4.49, targetFlow: 4.4, pressureDelta: 5, strokesPerMin: 60, hasFault: false },
-    { title: 'Well 2', tankLevel: 30, actualFlowRate: 4.20, targetFlow: 4.19, pressureDelta: 4, strokesPerMin: 55, hasFault: true },
-    { title: 'Well 3', tankLevel: 70, actualFlowRate: 3.92, targetFlow: 3.9, pressureDelta: 6, strokesPerMin: 70, hasFault: false },
-    { title: 'Well 4', tankLevel: 60, actualFlowRate: 3.82, targetFlow: 3.8, pressureDelta: 5, strokesPerMin: 65, hasFault: true },
-    { title: 'Well 5', tankLevel: 80, actualFlowRate: 4.97, targetFlow: 5, pressureDelta: 3, strokesPerMin: 75, hasFault: false },
-    { title: 'Well 6', tankLevel: 45, actualFlowRate: 4.43, targetFlow: 4.4,  pressureDelta: 4, strokesPerMin: 50, hasFault: true },
+    { title: 'Skid 1', tankLevel: 50, actualFlowRate: 4.49, targetFlow: 4.4, pressureDelta: 5, strokesPerMin: 60, hasFault: false },
+    { title: 'Skid 2', tankLevel: 30, actualFlowRate: 4.20, targetFlow: 4.19, pressureDelta: 4, strokesPerMin: 55, hasFault: true },
+    { title: 'Skid 3', tankLevel: 70, actualFlowRate: 3.92, targetFlow: 3.9, pressureDelta: 6, strokesPerMin: 70, hasFault: false },
+    { title: 'Skid 4', tankLevel: 60, actualFlowRate: 3.82, targetFlow: 3.8, pressureDelta: 5, strokesPerMin: 65, hasFault: true },
+    { title: 'Skid 5', tankLevel: 80, actualFlowRate: 4.97, targetFlow: 5, pressureDelta: 3, strokesPerMin: 75, hasFault: false },
+    { title: 'Skid 6', tankLevel: 45, actualFlowRate: 4.43, targetFlow: 4.4,  pressureDelta: 4, strokesPerMin: 50, hasFault: true },
   ];
 
   return (
